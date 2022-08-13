e038d01e7fbb83e4fd3fcf73904d25059919ff9b
KostyaBelyaev <bazaaa2008@gmail.com>
2022-08-13 22:39:51 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Studentinfo.java
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class Studentinfo {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Konstantin Belyaev");
         System.out.println("04.03.1987");
     }
