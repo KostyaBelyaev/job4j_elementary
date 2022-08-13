e69534039c74722a85ba8199f0f21626df3e4ee2
KostyaBelyaev <bazaaa2008@gmail.com>
2022-08-13 21:49:40 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args){
+        System.out.println("Konstantin Belyaev");
+        System.out.println("04.03.1987");
+    }
+}
