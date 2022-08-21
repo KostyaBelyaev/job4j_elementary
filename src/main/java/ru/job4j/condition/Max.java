42926e49ce2b0fb7e6afc0f4b9dbd248647fce3e
KostyaBelyaev <bazaaa2008@gmail.com>
2022-08-21 18:37:12 +0300

    4.2. Тернарное сравнение [#189]

---
+++ src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left > right ? left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int rsl = Max.max(10, 30);
+        System.out.println(rsl);
+    }
+}
+
