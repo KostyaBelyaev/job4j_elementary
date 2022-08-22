87fda5a154a3127135c68692538c1087d3c8a556
KostyaBelyaev <bazaaa2008@gmail.com>
2022-08-22 18:43:37 +0300

    4.3.2. Сокращенный оператор || [#333784]

---
+++ src/main/java/ru/job4j/condition/Club.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Club {
+    public static void premission(boolean hasMoney, boolean beFriend) {
+        if (hasMoney | beFriend) {
+            System.out.println("I can go to the club");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Club.premission(true, true);
+        Club.premission(true, false);
+        Club.premission(false, true);
+        Club.premission(false, false);
+    }
+}
+
+
